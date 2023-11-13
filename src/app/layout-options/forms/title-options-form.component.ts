70d7537f12f8eedb110e8a69e20344613044db6a
Jérémy Dutheil <dutheil.jeremy@gmail.com>
2023-11-13 11:14:16 +0100

    Adding textAlign for title

--- src/app/layout-options/forms/title-options-form.component.ts
+++ src/app/layout-options/forms/title-options-form.component.ts
@@ -6,13 +6,14 @@ import {
   Output,
 } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { RadioButtonModule } from 'primeng/radiobutton';
 import { SliderModule } from 'primeng/slider';
 import { Style } from '../../core/models/style';
 
 @Component({
   standalone: true,
   selector: 'title-options-form',
-  imports: [FormsModule, SliderModule],
+  imports: [FormsModule, SliderModule, RadioButtonModule],
   template: `
     <div>
       <div class="mb-3">
@@ -31,6 +32,41 @@ import { Style } from '../../core/models/style';
         />
       </div>
 
+      <div class="mb-3">
+        <label>Alignement</label>
+        <div class="flex flex-wrap gap-3">
+          <div class="flex align-items-center">
+            <p-radioButton
+              inputId="textAlignLeft"
+              name="textAlign"
+              value="left"
+              [(ngModel)]="textAlign"
+            ></p-radioButton>
+            <label for="textAlignLeft">Gauche</label>
+          </div>
+
+          <div class="flex align-items-center">
+            <p-radioButton
+              inputId="textAlignRight"
+              name="textAlign"
+              value="right"
+              [(ngModel)]="textAlign"
+            ></p-radioButton>
+            <label for="textAlignRight">Droite</label>
+          </div>
+
+          <div class="flex align-items-center">
+            <p-radioButton
+              inputId="textAlignCenter"
+              name="textAlign"
+              value="center"
+              [(ngModel)]="textAlign"
+            ></p-radioButton>
+            <label for="textAlignCenter">Centre</label>
+          </div>
+        </div>
+      </div>
+
       <button type="submit" class="btn btn-primary btn-sm" (click)="submit()">
         Save
       </button>
@@ -44,6 +80,7 @@ export class TitleOptionsFormComponent implements OnChanges {
   @Output() optionsSaved: EventEmitter<any> = new EventEmitter<any>();
 
   fontSize?: number;
+  textAlign?: string;
 
   ngOnChanges(): void {
     const fontSizeStyle = this.styles?.find(
@@ -52,6 +89,10 @@ export class TitleOptionsFormComponent implements OnChanges {
     this.fontSize = fontSizeStyle
       ? parseInt(fontSizeStyle.value.replace('px', ''))
       : 30;
+
+    this.textAlign =
+      this.styles?.find((style) => style.property === 'textAlign')?.value ??
+      'left';
   }
 
   submit(): void {
@@ -61,6 +102,11 @@ export class TitleOptionsFormComponent implements OnChanges {
         {
           property: 'fontSize',
           value: `${this.fontSize}px`,
+        },
+
+        {
+          property: 'textAlign',
+          value: this.textAlign,
         },
       ],
     });
